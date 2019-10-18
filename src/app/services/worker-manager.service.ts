6acc5e9fdb297376961cce9ea2dcf58c42db0173
jardmclan <mcleanj@hawaii.edu>
2019-10-17 17:31:07 -1000

    added notes on worker management structure

--- src/app/services/worker-manager.service.ts
+++ src/app/services/worker-manager.service.ts
@@ -13,33 +13,75 @@ export class WorkerManagerService {
 
   constructor() { }
 
-  getWorker() {
-    let worker = null;
-    //if max number of workers not hit create new worker
-    if(this.created < WorkerManagerService.MAX_WORKERS) {
-      //create WorkerHandler with new web worker
-    }
-    else {
-      if(this.suspended.length > 0) {
-        worker = this.suspended.shift();
+  // getWorker() {
+  //   let worker = null;
+  //   //if max number of workers not hit create new worker
+  //   if(this.created < WorkerManagerService.MAX_WORKERS) {
+  //     //create WorkerHandler with new web worker
+  //   }
+  //   else {
+  //     if(this.suspended.length > 0) {
+  //       worker = this.suspended.shift();
         
-      }
-    }
+  //     }
+  //   }
 
-    return new WorkerHandler(worker);
+  //   return new WorkerHandler(worker);
+  // }
+
+  //worker should exit if receive exit signal
+  private workerExecWrapper() {
+
   }
 }
 
 
+//each worker has a worker manager, worker handlers assigned to worker managers by the worker coordinator
+//worker coordinator should know how many handlers in which managers, which managers are busy, manager periodicity?
+//note workers can't have method replaced, need to stop original worker, then create new worker with new function
 
-export class WorkerHandler {
+
+//load balance worker managers
+class WorkerCoordinator {
+  Subject<WorkerUtilInfo>
+}
+
+interface WorkerUtilInfo {
+  handler: WorkerHandler;
+}
+
+//handles swapping between
+class WorkerManager {
+  worker: Worker;
+  removeF
+}
+
+export class WorkerHandler<T> {
   private workerExec;
+  private worker: Worker;
+  private manager: WorkerManager;
+
+  constructor(worker: Worker, workerExec) {
+
+    this.worker.onerror = () => {
+
+    };
+    this.worker.onmessage = () => {
+
+    };
+  }
+
+  send(data: T) {
+    //this.manager.sendMessageFromHandler(this);
+    this.worker.postMessage(data);
+  }
 
   //suspend worker, mark for priority (inactive) if a worker is needed for another process
   suspend() {
 
   }
 
+  //stop the worker, no longer used
   stop() {
 
   }
