048f72479242d8829a0de323cb11ae9c32c927ab
jardmclan <mcleanj@hawaii.edu>
2020-03-19 18:08:44 -1000

    creating validation for set select, splitting up selector

--- src/app/components/controls/data-set-interval-selector/data-set-interval-selector.component.ts
+++ src/app/components/controls/data-set-interval-selector/data-set-interval-selector.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, OnInit, Output, ViewChildren, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 //import {MatSelect} from "@angular/material/select";
 import {ClassModificationService} from "../../../services/controlHelpers/class-modification.service";
+import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-data-set-interval-selector',
@@ -94,11 +95,152 @@ export class DataSetIntervalSelectorComponent implements AfterViewInit {
   }
 }
 
+// interface DataSetComponents {
+//   setType: string,
+//   valueType: string,
+//   fill: string,
+//   timeRange: [string, string]
+// }
+
+//how to break this up?
+//granularity, fill, and set (including time range and type)
 interface DataSetComponents {
+  timeGranularity: string,
   setType: string,
-  valueType: string,
-  fill: string,
-  timeRange: [string, string]
+  fill: string
+}
+
+
+
+class DataSetCoordinator {
+
+  validValueSubjects: {
+    timeGranularity: Subject<string[]>,
+    setType: Subject<string[]>,
+    fill: Subject<string[]>
+  }
+
+  definitions: DataSetComponents[];
+
+  setValues: DataSetComponents;
+
+  constructor(validComponents: DataSetComponents[], defaultValues?: DataSetComponents) {
+    this.validValueSubjects = {
+      timeGranularity: new Subject<string[]>(),
+      setType: new Subject<string[]>(),
+      fill: new Subject<string[]>()
+    }
+    this.definitions = validComponents;
+
+    let nulledValues: DataSetComponents = {
+      timeGranularity: null,
+      setType: null,
+      fill: null
+    }
+    //set to nulled values if no default set
+    this.setValues = defaultValues == undefined ? nulledValues : JSON.parse(JSON.stringify(defaultValues));
+    //validate and set to null if invalid (no rule specified for default state)
+    if(!this.validate()) {
+      this.setValues = nulledValues;
+    }
+  }
+
+  validate(): boolean {
+    let nonNull = [];
+    let valid = false;
+    for(let component in this.setValues) {
+      if(this.setValues[component] !== null) {
+        nonNull.push(component);
+      }
+    }
+
+    if(nonNull.length == 0) {
+      valid = true;
+    }
+    else {
+      //check that non-null component combo in state matches some rule
+      for(let def in this.definitions) {
+        let match = true;
+        for(let component of nonNull) {
+          //rule does not match, set to false and break
+          if(this.setValues[component] != def[component]) {
+            match = false;
+            break;
+          }
+        }
+        //if found a matching rule then set valid and break, no need to keep looking
+        if(match) {
+          valid = true;
+          break;
+        }
+      }
+    }
+
+    return valid;
+  }
+
+  addDefinition(validComponent: DataSetComponents): void {
+    this.definitions.push(validComponent);
+  }
+
+  addDefinitions(validComponents: DataSetComponents[]): void {
+    this.definitions = this.definitions.concat(validComponents);
+  }
+
+  setComponent(component: keyof DataSetComponents, value: string): boolean {
+    let valid = true;
+    let temp = this.setValues[component];
+    this.setValues[component] = value;
+    //validate change to state
+    if(!this.validate()) {
+      valid = false;
+      //revert change
+      this.setValues[component] = temp;
+    }
+    else {
+      //get the valid values for the current state
+      let validComponents: ValidValues = this.getValidComponents();
+      //emit valid values
+      this.emitValidComponents(validComponents);
+    }
+    return valid;
+  }
+
+  private getValidComponents(): ValidValues {
+    return null;
+  }
+
+  private emitValidComponents(validComponents: ValidValues): void {
+    let values: any = {};
+    for(let component in validComponents) {
+      this.validValueSubjects[component].next(validComponents[component]);
+    }
+  }
+
+  getValidValuesObservables(): ValidValuesObservables {
+    let observables: ValidValuesObservables = {
+      timeGranularity: null,
+      setType: null,
+      fill: null
+    }
+    for(let subject in this.validValueSubjects) {
+      let observable = this.validValueSubjects[subject].toObservable();
+      observables[subject] = observable;
+    }
+    return observables;
+  }
+}
+
+interface ValidValues {
+  timeGranularity: string[],
+  setType: string[],
+  fill: string[]
+}
+
+interface ValidValuesObservables {
+  timeGranularity: Observable<string[]>,
+  setType: Observable<string[]>,
+  fill: Observable<string[]>
 }
 
 class DataSetInfoBuilder {
