92ccadb94e09e66fe1fd493ea103d97b80238bdc
princessbobsanta <princessbobsantaclaus@gmail.com>
2020-09-01 09:48:14 -1000

    fixed leaflet asset loading issue when building

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -123,6 +123,8 @@ export class MapComponent implements OnInit {
     //   });
     // }
 
+    delete (L.Icon.Default.prototype as any)._getIconUrl;
+
     L.Icon.Default.mergeOptions({
       iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
       iconUrl: require('leaflet/dist/images/marker-icon.png'),
