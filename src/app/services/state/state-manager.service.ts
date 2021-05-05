67cd1a8df7534b91fa3fe6a49fe00d8698875ced
princessbobsanta <princessbobsantaclaus@gmail.com>
2021-05-05 09:26:10 -1000

    state manager

---
+++ src/app/services/state/state-manager.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import Moment from "moment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StateManagerService {
+
+  //dataset composer
+
+  //dataset
+
+  //station select
+
+  constructor() { }
+}
+
+class StateManager {
+
+}
+
+//
+interface Timeseries {
+  
+  range: [Moment.Moment, Moment.Moment]
+}
