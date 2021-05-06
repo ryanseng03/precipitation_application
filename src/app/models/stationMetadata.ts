deea53784d6b81be3c107aea8aa05a3849307f81
princessbobsanta <princessbobsantaclaus@gmail.com>
2021-05-06 11:30:04 -1000

    state manager

--- src/app/models/stationMetadata.ts
+++ src/app/models/stationMetadata.ts
@@ -2,11 +2,53 @@
 import Moment from "moment";
 import * as L from "leaflet";
 
-export interface StationData {
-    datatype: string,
+//non dataset info
+export interface StationValue {
+    metadata: string,
     id: string,
     date: Moment.Moment,
     value: number,
-    location: L.LatLng
-    metadata: {[field: string]: string}
+    location: L.LatLng,
+    //metadata: {}
 };
+
+export interface StationMetadata {
+    id: string,
+    location: L.LatLng,
+    ext: {
+        [field: string]: string | number
+    }
+    
+}
+
+export interface StationData {
+
+}
+
+export interface StationFieldMetadata {
+    type: StationFieldType,
+
+}
+
+export abstract class StationField {
+    type: StationFieldType;
+    value: string | number;
+    constructor(type: StationFieldType) {
+        this.type = type;
+    }
+    
+}
+
+export class DiscreetStationField {
+
+}
+
+export type StationFieldType = "discreet" | "continuous";
+
+//
+
+export interface Dataset {
+    datatype: string,
+    period: string,
+    tier: number
+}
