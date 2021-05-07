12af05db58d6d9d77f75513fef46e2f2fd502a24
jardmclan <mcleanj@hawaii.edu>
2021-05-07 11:09:18 -1000

    dataset

--- src/app/models/dataset.ts
+++ src/app/models/dataset.ts
@@ -8,6 +8,14 @@ export interface Dataset {
     type: SetType
 }
 
+
+//should move this, this is a generic description item
+export interface ValueData<T> {
+  value: T,
+  label: string,
+  description: string
+}
+
 export type FillType = "partial" | "full" | "none";
 export type Timestep = "monthly" | "daily";
 export type SetType = "rainfall" | "temperature";
